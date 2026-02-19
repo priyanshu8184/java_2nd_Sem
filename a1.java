dbc340245ebb8d6f6d6bd8d54994eed457d9a358
priyanshu8184 <priyanshu74164@gmail.com>
2026-02-19 09:42:42 +0530

    small programs

---
+++ a1.java
@@ -0,0 +1,16 @@
+public class a1 {
+    public static void main(String[]args){
+        int x=8;
+        int y=3;
+        int z=2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x%y);
+        x=5;
+        ++z;
+        System.out.println(x);
+        --z;
+        System.out.println(z);
+    }
+}
